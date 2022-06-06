d550e6c3c37e4f20670894516f7e0c324bb5df14
Lazar Sidor <lazar.sidor@gmail.com>
2022-06-06 12:28:34 +0300

    Removed authentication related module

--- Package.swift
+++ Package.swift
@@ -11,38 +11,11 @@ let package = Package(
         .library(
             name: "iosplusNetworking",
             targets: ["iosplusNetworking"]
-        ),
-        .library(
-            name: "iosplusFirebaseAuthentication",
-            targets: ["iosplusCoreAuthentication", "iosplusFirebaseAuthentication"]
-        ),
-        .library(
-            name: "iosplusAwsCognitoAuthentication",
-            targets: ["iosplusCoreAuthentication", "iosplusAwsCognitoAuthentication"]
         )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(
-          name: "Firebase",
-          url: "https://github.com/firebase/firebase-ios-sdk",
-          "8.0.0" ..< "9.0.0"
-        ),
-        .package(
-          name: "Facebook",
-          url: "https://github.com/facebook/facebook-ios-sdk",
-          "12.2.1" ..< "13.0.0"
-        ),
-        .package(
-          name: "GoogleSignIn",
-          url: "https://github.com/google/GoogleSignIn-iOS",
-          "6.1.0" ..< "7.0.0"
-        ),
-        .package(
-          name: "Amplify",
-          url: "https://github.com/aws-amplify/amplify-ios",
-          "1.0.0" ..< "2.0.0"
-        )
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -51,29 +24,6 @@ let package = Package(
             name: "iosplusNetworking",
             dependencies: [],
             path: "Sources/iosplusNetworking"
-        ),
-        .target(
-            name: "iosplusCoreAuthentication",
-            dependencies: [],
-            path: "Sources/iosplusAuthentication/Core"
-        ),
-        .target(
-            name: "iosplusFirebaseAuthentication",
-            dependencies: [
-                "iosplusCoreAuthentication",
-                .product(name: "FirebaseAuth", package: "Firebase"),
-                .product(name: "FacebookLogin", package: "Facebook"),
-                "GoogleSignIn"
-            ],
-            path: "Sources/iosplusAuthentication/Firebase"
-        ),
-        .target(
-            name: "iosplusAwsCognitoAuthentication",
-            dependencies: [
-                "iosplusCoreAuthentication",
-                .product(name: "AWSCognitoAuthPlugin", package: "Amplify"),
-            ],
-            path: "Sources/iosplusAuthentication/Amplify"
         ),
         .testTarget(
             name: "iosplusNetworkingTests",
--- README.md
+++ README.md
@@ -8,7 +8,3 @@ XCFramework providing helper classes to perform:
 - Helpers for standard HTTP operations using Foundation/URLSession based mechanism and with response serialisation/ deserialisation using Swift Codable protocol
 
 
-# iosplusAuthentication target
-- Offers basic authentication services with different Identity providers (e.g: Firebase, Facebook, Google, AWS Cognito)
-
-
